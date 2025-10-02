e793172d422a99bc7256682997f89152e0337b38
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-10-02 20:17:12 -0300

    initial

---
+++ src/main/java/com/example/projeto_bd3_prova3/ProjetoBd3Prova3Application.java
@@ -0,0 +1,13 @@
+package com.example.projeto_bd3_prova3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBd3Prova3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBd3Prova3Application.class, args);
+	}
+
+}
